398646db2f8ab9cc91d34b951918959bbe3548fe
Isaiah Chastain <ipchastain42@tntech.edu>
2026-08-27 13:13:06 -0500

    Dockerized app

--- app/storage.py
+++ app/storage.py
@@ -1,11 +1,18 @@
 import json
 from pathlib import Path
+import os
 
-TASKS_FILE = Path("app/tasks.json")
+TASKS_FILE = Path(
+    os.getenv("TASKS_FILE", "app/tasks.json")
+)
 
 def load_tasks(file_path=None):
     if file_path is None:
         file_path = TASKS_FILE
+
+    if not file_path.exists():
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        file_path.write_text("[]")
 
     with open(file_path, "r") as file:
         return json.load(file)
